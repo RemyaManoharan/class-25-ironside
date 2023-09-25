896c3f79a4112aa3517dddcd0545e884bf9eb674
sayali <hackyourfuturedk@HackYourFutures-MacBook-Air.local>
2023-09-25 14:21:38 +0200

    style for forms

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { Box, Grid, Typography, TextField, Button, InputLabel,Checkbox,
   FormControlLabel, } from "@mui/material";
-import Login from "../../assets/login.png";
-import Icon from "../../assets/aperture.png";
+import LoginImage from "../../assets/login.svg";
+import Icon from "../../assets/aperture.svg";
 import "./Login.css";
 
 function LoginPage() {
@@ -34,94 +34,96 @@ function LoginPage() {
     } catch (error) {
       console.error("Error during login:", error);
 
-      setError("failed to login check your info");
+      setError("Failed to login check your info");
     }
 
     setLoading(false);
   };
-
-  const [isButtonClicked, setIsButtonClicked] = useState(false);
-  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const isButtonDisabled = !email || !password || loading;
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={3}>
-        {/* Left Column */}
-        <Grid item xs={12} md={6} className="forgetpass-column">
-          <Box height="100vh" padding={5}>
-            <Typography component="p" variant="h5">
-            <img className="Icon" src={Icon} alt="Icon" /> Dashboard
-            </Typography>
-            <div className="forgetpass-container">
-              
-              <div className="arrow-icon">
-            
-                <>
-                  <Typography variant="subtitle2" className="forget-password">Login</Typography>
-                  <Typography variant="subtitle1">Easy step to enter the platform</Typography>
-                  {error && <p>{error}</p>}
-                  <form onSubmit={handleSubmit}>
-                  <div className="label-wrap">
-                    <InputLabel className="label">Email</InputLabel>
-                    <TextField
-                      name="email"
-                      id="outlined-required"
-                      label="Enter email"
-                      variant="outlined"
-                      className="email-input"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </div>
-                  <div className="label-wrap">
-                    <InputLabel className="label">Password</InputLabel>
-                    <TextField
-                      name="password"
-                      id="outlined-required"
-                      label="Enter password"
-                      variant="outlined"
-                      className="email-input"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                  </div>
-                  <FormControlLabel
-                control={<Checkbox name="rem-password" value="rem-password" />}
-                label="Remember me"
-              />
-              <Typography variant="body1">
-                  <Link to="/password">Forget Password</Link>
-              </Typography>
-                  <div className="button-wrap">
-                    <Button
-                      variant="contained"
-                      className={`reset-password-button ${isButtonDisabled ? "disabled" : ""}`}
-                      disabled={loading} type="submit"
-                      >
-                     Log in
-                    </Button>
-                  </div>
-                </form> 
-                  <Typography variant="body1">
-                  Don’t have an account?<Link to="/signup">Sign Up</Link>
-              </Typography>
-                </>
-              
-            </div>
-            </div>   
-          </Box>
-        </Grid>
 
-        {/* Right Column */}
-        <Grid item xs={12} md={6} className="image-column">
-        <Box height="100vh" marginTop={5}>
-                <img className="login" src={Login} alt="Login" />
-              </Box>
-        </Grid>
-      </Grid>
-    </Box>
-  
-  );
+    <div className="main-container">
+    <div className="header">
+    <Typography component="p" variant="h5">
+                <img className="Icon" src={Icon} alt="Icon" /> Dashboard
+              </Typography>
+    </div>
+    <div className="main-content">
+      <div className="form-content">
+        <div className="form-design">
+          <Typography variant="subtitle2" className="login-title">
+            Log in
+          </Typography>
+          <Typography variant="subtitle1">Easy steps to enter the platform</Typography>
+          {error && <p className="error-message">{error}</p>}
+          <form onSubmit={handleSubmit}>
+            <div className="label-wrap">
+              <InputLabel className="label">Email</InputLabel>
+              <TextField
+                name="email"
+                id="email"
+                label="Enter email"
+                variant="outlined"
+                className="email-input"
+                InputProps={{ style: { background: '#F8F9FD' } }} 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="label-wrap">
+            <InputLabel className="label">Password</InputLabel>
+              <TextField
+                name="password"
+                id="password"
+                label="Enter password"
+                variant="outlined"
+                className="email-input"
+                InputProps={{ style: { background: '#F8F9FD' } }} 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <FormControlLabel
+              control={<Checkbox name="rem-password" value="rem-password" />}
+              label="Remember me"
+            />
+             <div className="button-wrap">
+            <Typography variant="body1" className="forget-pass">
+              <Link to="/password">Forgot Password</Link>
+            </Typography>
+           
+              <Button
+                variant="contained"
+                className={`login-button ${loading ? "disabled" : ""}`}
+                disabled={isButtonDisabled}
+                type="submit"
+                style={{
+                  width: '60%', 
+                  height: '10%', 
+                  padding: '2%', 
+                  borderRadius: '4px',
+                  cursor: 'pointer',
+                  fontSize: '16px'
+                }}
+              >
+                Log in
+              </Button>
+            </div>
+          </form>
+        </div>
+        <div className="form-span">
+          <Typography variant="body1">
+            Don’t have an account? <Link to="/signup">Sign Up</Link>
+          </Typography>
+        </div>
+      </div>
+      <div className="image-content">
+        <img className="login-image" src={LoginImage} alt="Login" />
+      </div>
+    </div>
+  </div>
+);
 }
 
 export default LoginPage;
--- frontend/src/pages/signup/Signup.tsx
+++ frontend/src/pages/signup/Signup.tsx
@@ -12,7 +12,8 @@ import {
   FormControlLabel,
   InputLabel,
 } from "@mui/material";
-import signupImage from "../../assets/signup.png";
+import signupImage from "../../assets/signup.svg";
+import Icon from "../../assets/aperture.svg";
 import "./Signup.css";
 export default function SignUpPage() {
   const [email, setEmail] = useState<string>("");
@@ -24,12 +25,12 @@ export default function SignUpPage() {
   const [loading, setLoading] = useState(false);
   const { signup, updateUser } = useContext(AuthContext);
   const navigate = useNavigate();
+
+  const isButtonDisabled = !email || !password || loading;
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     console.log("submit");
     e.preventDefault();
-    if(password.length < 8){
-      return setError("Password should be more than 8 characters");
-    }
     if (password !== passwordConfirm) {
       return setError("Passwords do not match");
     }
@@ -46,169 +47,291 @@ export default function SignUpPage() {
     setLoading(false);
   };
   return (
-    <>
-      <Box
-        sx={{
-          flexGrow: 1,
-          maxWidth: 1300,
-          margin: "0 auto",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <Grid container spacing={1}>
-          {/* Left Column */}
-          <Grid item xs={12} md={6} className="signup-column">
-            <Box height="100vh" padding={5}>
-              <Typography component="p" variant="h5">
-                Dashboard
-              </Typography>
-              <div className="signup-container">
-                <Typography variant="h6">Sign Up</Typography>
-                <Typography variant="subtitle1">Select subscription</Typography>
-                {error && <p>{error}</p>}
-                <form onSubmit={handleSubmit}>
-                  <div className="name-wrapper">
-                    <div className="label-wrap">
-                      <InputLabel className="label">First name</InputLabel>
-                      <TextField
-                        name="fName"
-                        id="outlined-required"
-                        label="First Name"
-                        variant="outlined"
-                        type="text"
-                        size="small"
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)}
-                        required
-                        sx={{
-                          width: { sm: 120, md: 120 },
-                          "& .MuiInputBase-root": {
-                            height: 50,
-                          },
-                        }}
-                      />
-                    </div>
-                    <div className="label-wrap">
-                      <InputLabel className="label">Last name</InputLabel>
-                      <TextField
-                        name="lName"
-                        id="outlined-required"
-                        label="Last Name"
-                        type="text"
-                        variant="outlined"
-                        required
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)}
-                        sx={{
-                          width: { sm: 120, md: 120 },
-                          "& .MuiInputBase-root": {
-                            height: 50,
-                          },
-                        }}
-                      />
-                    </div>
-                  </div>
+    <div className="main-container">
+      <div className="header">
+      <Typography component="p" variant="h5">
+                  <img className="Icon" src={Icon} alt="Icon" /> Dashboard
+                </Typography>
+      </div>
+      <div className="main-content">
+        <div className="form-content">
+            <div className="form-design">
+              <Typography variant="subtitle2" className="login-title">
+               Sign up
+              </Typography>              
+              {error && <p>{error}</p>}
+              <form onSubmit={handleSubmit}>
+                <div className="name-wrapper">
                   <div className="label-wrap">
-                    <InputLabel className="label">Email</InputLabel>
+                    <InputLabel className="label">First name</InputLabel>
                     <TextField
-                      name="email"
+                      name="fName"
                       id="outlined-required"
-                      label="Enter email"
+                      label="First Name"
                       variant="outlined"
-                      type="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
+                      type="text"
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
+                      InputProps={{ style: { background: '#F8F9FD' } }} 
                       required
-                      sx={{
-                        width: { sm: 273, md: 273 },
-                        "& .MuiInputBase-root": {
-                          height: 50,
-                        },
-                      }}
+                      className="email-input" 
+                      
                     />
                   </div>
                   <div className="label-wrap">
-                    <InputLabel className="label">Password</InputLabel>
+                    <InputLabel className="label">Last name</InputLabel>
                     <TextField
-                      name="password"
+                      name="lName"
                       id="outlined-required"
-                      label="Enter password"
+                      label="Last Name"
+                      type="text"
                       variant="outlined"
-                      type="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
                       required
-                      sx={{
-                        width: { sm: 273, md: 273 },
-                        "& .MuiInputBase-root": {
-                          height: 50,
-                        },
-                      }}
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
+                      InputProps={{ style: { background: '#F8F9FD' } }} 
+                      className="email-input"
                     />
                   </div>
-                  <div className="label-wrap">
-                    <InputLabel className="label">Confirm Password</InputLabel>
-                    <TextField
-                      name="passwordConfirm"
-                      id="outlined-required"
-                      label="Confirm Password"
-                      variant="outlined"
-                      type="password"
-                      value={passwordConfirm}
-                      onChange={(e) => setPasswordConfirm(e.target.value)}
-                      required
-                      sx={{
-                        width: { sm: 273, md: 273 },
-                        "& .MuiInputBase-root": {
-                          height: 50,
-                        },
-                      }}
-                    />
-                  </div>
-                  <FormControlLabel
-                    control={
-                      <Checkbox name="rem-password" value="rem-password" />
-                    }
-                    label="Remember me"
+                </div>
+                <div className="label-wrap">
+                  <InputLabel className="label">Email</InputLabel>
+                  <TextField
+                    name="email"
+                    id="outlined-required"
+                    label="Enter email"
+                    variant="outlined"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    InputProps={{ style: { background: '#F8F9FD' } }} 
+                    required
+                    className="email-input"
                   />
-                  <div>
-                    <button disabled={loading} type="submit" className="btn">
-                      Sign Up
-                    </button>
+                </div>
+                <div className="label-wrap">
+                  <InputLabel className="label">Password</InputLabel>
+                  <TextField
+                    name="password"
+                    id="outlined-required"
+                    label="Enter password"
+                    variant="outlined"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    InputProps={{ style: { background: '#F8F9FD' } }} 
+                    required
+                    className="email-input"
+                  />
+                </div>
+                <div className="label-wrap">
+                  <InputLabel className="label">Confirm Password</InputLabel>
+                  <TextField
+                    name="passwordConfirm"
+                    id="outlined-required"
+                    label="Confirm Password"
+                    variant="outlined"
+                    type="password"
+                    value={passwordConfirm}
+                    onChange={(e) => setPasswordConfirm(e.target.value)}
+                    InputProps={{ style: { background: '#F8F9FD' } }} 
+                    required
+                    className="email-input"
+                  />
+                </div>
+                
+                <div>
+                <Button
+                variant="contained"
+                className={`signup-button ${loading ? "disabled" : ""}`}
+                disabled={isButtonDisabled}
+                type="submit"
+                style={{
+                  width: '60%', 
+                  height: '10%', 
+                  padding: '2%', 
+                  borderRadius: '4px',
+                  cursor: 'pointer',
+                  fontSize: '16px'
+                }}
+              >
+                Sign up
+              </Button>
+                  
+                </div>
+                <div className="form-span">
+          <Typography variant="body1">
+          Already have an account? <Link to="/login">Sign In</Link>
+          </Typography>
+        </div>
+                
+              </form>
+            </div>
+        </div>
+        <div className="image-content">
+        <img className="login-image" src={signupImage} alt="Login" />
+      </div>
+      </div>
+  </div>
+         
+
+   /* <Box
+      sx={{
+        flexGrow: 1,
+        maxWidth: 1300,
+        margin: "0 auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        
+      }}
+    >
+      <Grid container spacing={1}>
+      
+        <Grid item xs={12} md={6} className="signup-column">
+            
+          <Box padding={5} className="signup-content"> 
+            <Typography component="p" variant="h5">
+            <img className="Icon" src={Icon} alt="Icon" />  Dashboard
+            </Typography>
+            <div className="signup-container">
+              <Typography variant="h6">Sign Up</Typography>
+              <Typography variant="subtitle1">Select subscription</Typography>
+              {error && <p>{error}</p>}
+              <form onSubmit={handleSubmit}>
+                <div className="name-wrapper">
+                  <div className="label-wrap">
+                    <InputLabel className="label">First name</InputLabel>
+                    <TextField
+                      name="fName"
+                      id="outlined-required"
+                      label="First Name"
+                      variant="outlined"
+                      type="text"
+                      size="small"
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
+                      required
+                      sx={{
+                        width: { sm: 120, md: 120 },
+                        "& .MuiInputBase-root": {
+                          height: 50,
+                        },
+                      }}
+                    />
                   </div>
-                  <div>
-                    <Typography variant="subtitle1">
-                      Already have an account?
-                    </Typography>
-                    <Link to="/login">Log In</Link>
+                  <div className="label-wrap">
+                    <InputLabel className="label">Last name</InputLabel>
+                    <TextField
+                      name="lName"
+                      id="outlined-required"
+                      label="Last Name"
+                      type="text"
+                      variant="outlined"
+                      required
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
+                      sx={{
+                        width: { sm: 120, md: 120 },
+                        "& .MuiInputBase-root": {
+                          height: 50,
+                        },
+                      }}
+                    />
                   </div>
-                </form>
-              </div>
-            </Box>
-          </Grid>
-          {/* Right Column */}
-          <Grid item xs={12} md={6} className="image-column">
-            <Box height="100vh" marginTop={5}>
-              <img className="handimage" src={signupImage} alt="Home" />
-            </Box>
-          </Grid>
+                </div>
+                <div className="label-wrap">
+                  <InputLabel className="label">Email</InputLabel>
+                  <TextField
+                    name="email"
+                    id="outlined-required"
+                    label="Enter email"
+                    variant="outlined"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    sx={{
+                      width: { sm: 273, md: 273 },
+                      "& .MuiInputBase-root": {
+                        height: 50,
+                      },
+                    }}
+                  />
+                </div>
+                <div className="label-wrap">
+                  <InputLabel className="label">Password</InputLabel>
+                  <TextField
+                    name="password"
+                    id="outlined-required"
+                    label="Enter password"
+                    variant="outlined"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    sx={{
+                      width: { sm: 273, md: 273 },
+                      "& .MuiInputBase-root": {
+                        height: 50,
+                      },
+                    }}
+                  />
+                </div>
+                <div className="label-wrap">
+                  <InputLabel className="label">Confirm Password</InputLabel>
+                  <TextField
+                    name="passwordConfirm"
+                    id="outlined-required"
+                    label="Confirm Password"
+                    variant="outlined"
+                    type="password"
+                    value={passwordConfirm}
+                    onChange={(e) => setPasswordConfirm(e.target.value)}
+                    required
+                    sx={{
+                      width: { sm: 273, md: 273 },
+                      "& .MuiInputBase-root": {
+                        height: 50,
+                      },
+                    }}
+                  />
+                </div>
+                <FormControlLabel
+                  control={
+                    <Checkbox name="rem-password" value="rem-password" />
+                  }
+                  label="Remember me"
+                />
+                <div>
+                  <button disabled={loading} type="submit" className="btn">
+                    Sign Up
+                  </button>
+                </div>
+                <div>
+                  <Typography variant="subtitle1">
+                    Already have an account?
+                  </Typography>
+                  <Link to="/login">Log In</Link>
+                </div>
+              </form>
+            </div>
+          </Box>
         </Grid>
-      </Box>
-    </>
+
+        <Grid item xs={12} md={6} className="image-column">
+          <Box
+            className="handimage"
+            sx={{
+              background: `url(${signupImage})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "contain",
+            }}
+            marginTop={5}
+          /> 
+        </Grid>
+      </Grid>
+    </Box> */
   );
 }
-
-
-
-
-
-
-
-
-
-
-
