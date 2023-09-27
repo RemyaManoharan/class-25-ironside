c20143c6c22de4f3c89bfa66a361703769699a0b
Petr Brantalík <grounipteam@gmail.com>
2023-09-27 10:28:27 +0200

    fix: test lint staged

--- frontend/src/pages/main/Main.tsx
+++ frontend/src/pages/main/Main.tsx
@@ -1,37 +1,34 @@
-import React, { useState } from "react";
-import Header from "../../components/header/Header";
-import NavBar from "../../components/navbar/Navbar";
-import { Outlet } from "react-router-dom";
-import menu from "../../assets/menu.svg";
-import "./main.css";
+import React, { useState } from 'react';
+import Header from '../../components/header/Header';
+import NavBar from '../../components/navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+import menu from '../../assets/menu.svg';
+import './main.css';
 
 function Main() {
   const [showNav, setShowNav] = useState(false);
-
+  console.log('assad');
   const toggleNav = () => {
     setShowNav(!showNav);
   };
 
   return (
-    <section className="grid-container">
-      <button
-        className={`toggle-button ${showNav ? "hidden" : ""}`}
-        onClick={toggleNav}
-      >
-        <img src={menu} alt="menu" />
+    <section className='grid-container'>
+      <button className={`toggle-button ${showNav ? 'hidden' : ''}`} onClick={toggleNav}>
+        <img src={menu} alt='menu' />
       </button>
 
-      <nav className={`nav ${showNav ? "nav-show" : ""}`}>
+      <nav className={`nav ${showNav ? 'nav-show' : ''}`}>
         <NavBar />
       </nav>
 
-      <div className="header-outlet-container">
-        <div className="grid-display">
-          <header className="header">
+      <div className='header-outlet-container'>
+        <div className='grid-display'>
+          <header className='header'>
             <Header />
           </header>
 
-          <div className="outlet">
+          <div className='outlet'>
             <Outlet />
           </div>
         </div>
