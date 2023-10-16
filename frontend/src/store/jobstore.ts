f45391c04232d4028d7d7b7c06fc4f09fe8427db
RemyaManoharan <remyajinil@outlook.com>
2023-10-16 13:15:40 +0200

    changes in jobcard

--- frontend/src/store/jobstore.ts
+++ frontend/src/store/jobstore.ts
@@ -45,12 +45,12 @@ interface JobStore {
   }) => void;
 
   submitJobApplication: (jobId: number, applicationData: JobApplication) => Promise<void>;
-  fetchTotalJobApplications: (jobId: number) => Promise<void>;
+  fetchTotalJobApplications: (jobId: number) => Promise<any>;
 }
 
 const useJobStore = create<JobStore>((set, get) => ({
   jobs: [],
-  totalJobApplicants: 0,
+  totalJobApplicants: 3333,
   filters: {
     location: '',
     workTypes: [],
@@ -116,7 +116,6 @@ const useJobStore = create<JobStore>((set, get) => ({
       const response = await request.get(`/jobs/${jobId}/count`);
       const totalApplicants = (await response.data[0].total_applicants) as number;
       set({ totalJobApplicants: totalApplicants });
-      console.log(`Total Applicants for job ${jobId}:`, totalApplicants);
     } catch (error) {
       console.error('Error fetching total job applicants:', error);
       throw new Error('Error fetching total job applicants.');
